c78c630487a7da3fac2d50dd29074cd7653103d9
adelola <lola.adekunle@gmail.com>
2019-04-25 23:54:55 -0500

    Add responsiveness to navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -26,33 +26,151 @@ const Navigation = styled.header`
   a:hover {
     opacity: .5;
   }
+  .fa-bars {
+    display: none;
+    color: #222;
+    font-size: 2rem;
+  }
+  nav {
+    ul {
+      display: flex;
+      justify-content: space-between;
+      list-style: none;
+    }
+    li {
+      margin: 0 15px;
+      justify-content: space-between;
+      font-size: 1em;
+    }
+    a {
+      font-size: 1em;
+      text-decoration: none;
+      .active {
+        color: #222;
+      }
+    }
+  }
+  @media only screen and (max-width: 800px) {
+    padding: 0px;
+    .logo {
+      padding-left: 15px;
+      padding-top: 0px !important;
+    }
+  }
+  @media only screen and (max-width: 600px) {
+    height: auto;
+    min-height: 50px;
+    display: block;
+    position: relative;
+    .logo {
+      width: 100%;
+      display: block;
+      padding-top: 20px;
+      margin: 0px;
+      margin-left: -5px;
+      a {
+        padding: 20px 0px;
+      }
+    }
+    .fa-bars {
+      display: inline-block;
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      cursor: pointer;
+    }
+    ul.collapsed {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      flex-wrap: wrap;
 
+      overflow: hidden;
+      max-height: 0;
+      -moz-transition-duration: 0.4s;
+      -webkit-transition-duration: 0.4s;
+      -o-transition-duration: 0.4s;
+      transition-duration: 0.4s;
+      -moz-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+      -webkit-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+      -o-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+      transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
 
+      &.is-expanded {
+        overflow: hidden;
+        max-height: 500px; /* approximate max height */
+        -moz-transition-duration: 0.4s;
+        -webkit-transition-duration: 0.4s;
+        -o-transition-duration: 0.4s;
+        transition-duration: 0.4s;
+        -moz-transition-timing-function: ease-in;
+        -webkit-transition-timing-function: ease-in;
+        -o-transition-timing-function: ease-in;
+        transition-timing-function: ease-in;
+      }
+      li {
+        padding: 15px 10px;
+        margin: 0px 0px;
+        width: 100%;
+      }
+    }
+  }
 `;
 
 
 class Nav extends React.Component{
-    
-    render(){
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false
+    };
+  }
+  handleToggle(e) {
+    e.preventDefault();
+    this.setState({
+      isExpanded: !this.state.isExpanded
+    });
+  }
+
+  render(){
       // const { anchorLinks } = this.props
       const anchorLinks = [{ url: "#about", name:"About"}, { url: "#experience", name:"Experience"}, { url: "#mission", name:"Mission"}, { url: "#projects", name:"Projects"} ]
     return (
-    <Navigation>
-        <Link to="/">
-          Adelola Adekunle
-        </Link>
-          { anchorLinks.map(({ url, name }, i) => (
-            <div key={i} >
-              <AnchorLink href={url}>{name}</AnchorLink>
-            </div>
-          ))}
-    </Navigation>
+      <Navigation>
+        <div className="logo">
+          <Link to="/">
+            Logo
+          </Link>
+        </div>
+        <nav className="nav">
+          <i  
+            className="fa fa-bars"
+            aria-hidden="true"
+            onClick={e => this.handleToggle(e)}
+          />
+          <ul className={`collapsed ${this.props.isExpanded ? "is-expanded" : ""}`}>
+            { anchorLinks.map(({ url, name }, i) => (
+              <li key={i}>
+                <AnchorLink href={url}>{name}</AnchorLink>
+              </li>
+              ))}
+          </ul>
+        </nav>
+      </Navigation>
     )
   }
 }
 
 
-  
+
+
+
+
+
+
+
+
 
 // Nav.propTypes = {
 //   siteTitle: PropTypes.string,
