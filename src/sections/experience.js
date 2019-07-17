98f120b84b4cb9e309919692c1435d97d33a5885
adelola <lola.adekunle@gmail.com>
2019-07-16 23:52:59 -0400

    Add scrollmagic

--- src/sections/experience.js
+++ src/sections/experience.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Img from 'gatsby-image'
-import { TweenMax  } from "gsap/TweenMax"
+import { TweenMax  } from 'gsap/TweenMax'
 import styled from '@emotion/styled'
 import colors from '../styles/colors'
-import { propTypes } from 'react-typography/dist/GoogleFont';
+import ScrollMagic from 'scrollmagic'
+import { propTypes } from 'react-typography/dist/GoogleFont'
 
 const ExperienceStyle = styled.div`
   padding: 2em;
@@ -12,14 +13,51 @@ const ExperienceStyle = styled.div`
 `;
 
 class Experience extends React.Component {
-    
+  constructor(props) {
+    super(props);
+    this.state = { images: props.sequence }
+    this.controller = new ScrollMagic.Controller();
+  }
+
+  componentDidMount() {
+    // TweenMax can tween any property of any object. We use this object to cycle through the array
+    const obj = {curImg: 0};
+    const images = this.state.images;
+
+    // create tween
+    const tween = TweenMax.to(obj, 0.5,
+      {
+        curImg: images.length - 1,	// animate propery curImg to number of images
+        roundProps: "curImg",				// only integers so it can be used as an array index
+        repeat: 3,									// repeat 3 times
+        immediateRender: true,			// load first image automatically
+        ease: Linear.easeNone,			// show every image the same ammount of time
+        onUpdate: function () {
+          // $("#myimg").attr("src", images[obj.curImg])
+        }
+      }
+    );
+
+    new ScrollMagic.Scene({
+      triggerElement: "#trigger",
+      duration: 600, // scroll distance
+    })
+      .setTween(tween)
+      .addTo(this.controller); // assign the scene to the controller
+  }
+
     render() {
-      const images= this.props.sequence
+      
 
     return (
-    <ExperienceStyle id="experience"> 
+    <ExperienceStyle id="trigger"> 
       <h1>Experience</h1>
       <div id="imagesequence" >
+        {this.state.images.map(( image ) => {
+          return (
+            <Img fluid={image.fluid}/>
+          )})
+        }  
       </div>
     </ExperienceStyle > 
       )
